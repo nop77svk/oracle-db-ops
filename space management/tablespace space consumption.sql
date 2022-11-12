04f4a3c643d48145f74e17834c17eef6bb349983
Peter Hraško <peter_hrasko@ri-rpc.sk>
2022-11-12 19:43:27 +0100

    🛠 query enhanced with number of segments contained within a tablespace

--- space management/tablespace space consumption.sql
+++ space management/tablespace space consumption.sql
@@ -1,21 +1,30 @@
 select
-    fs.tablespace_name,
+    tablespace_name,
+    df.total_gb,
     (df.total_gb - fs.free_gb) as used_gb,
     fs.free_gb,
-    df.total_gb,
-    round(100 * (fs.free_gb / nullif(df.total_gb, 0))) as pct_free
+    round(100 * (fs.free_gb / nullif(df.total_gb, 0))) as pct_free,
+    S.segs#
 from (
         select tablespace_name,
-            round(sum(bytes) / 1048576 / 1024) as total_gb
+            round(sum(bytes) / 1048576 / 1024, 2) as total_gb
         from dba_data_files
+--        where tablespace_name like '...' -- [in] tablespace name
         group by tablespace_name
     ) df
-    join (
+    full outer join (
         select tablespace_name,
-            round(sum(bytes) / 1048576 / 1024) as free_gb
+            round(sum(bytes) / 1048576 / 1024, 2) as free_gb
         from dba_free_space
 --        where tablespace_name like '...' -- [in] tablespace name
         group by tablespace_name     
     ) fs
-        on fs.tablespace_name = df.tablespace_name
+        using (tablespace_name)
+    full outer join (
+        select tablespace_name, count(1) as segs#
+        from dba_segments
+--        where tablespace_name like '...' -- [in] tablespace name
+        group by tablespace_name
+    ) S
+        using (tablespace_name)
 order by free_gb desc;
