863125b37260a308f6a3a7a7f2fc77d10def7dd8
Peter Hraško <peter_hrasko@ri-rpc.sk>
2022-12-14 14:18:39 +0100

    ➖ size rounding removed

--- space management/tablespace space consumption.sql
+++ space management/tablespace space consumption.sql
@@ -1,30 +1,32 @@
-select
+select --+ no_merge(DF) no_merge(FS) no_merge(S)
     tablespace_name,
-    df.total_gb,
-    (df.total_gb - fs.free_gb) as used_gb,
-    fs.free_gb,
-    round(100 * (fs.free_gb / nullif(df.total_gb, 0))) as pct_free,
-    S.segs#
+    round(df.total_gb, 2) as total_gb,
+    round(df.total_gb - fs.free_gb, 2) as used_gb,
+    round(fs.free_gb, 2) as free_gb,
+    round(100 * (fs.free_gb / nullif(df.total_gb, 0))) as free_pct,
+    S.segs#, round(S.segs_gb, 2) as segs_gb
 from (
         select tablespace_name,
-            round(sum(bytes) / 1048576 / 1024, 2) as total_gb
+            sum(bytes) / 1048576 / 1024 as total_gb
         from dba_data_files
 --        where tablespace_name like '...' -- [in] tablespace name
         group by tablespace_name
     ) df
     full outer join (
         select tablespace_name,
-            round(sum(bytes) / 1048576 / 1024, 2) as free_gb
+            sum(bytes) / 1048576 / 1024 as free_gb
         from dba_free_space
 --        where tablespace_name like '...' -- [in] tablespace name
-        group by tablespace_name     
+        group by tablespace_name
     ) fs
         using (tablespace_name)
     full outer join (
-        select tablespace_name, count(1) as segs#
+        select tablespace_name, count(1) as segs#, sum(bytes / 1048576 / 1024) as segs_gb
         from dba_segments
 --        where tablespace_name like '...' -- [in] tablespace name
         group by tablespace_name
     ) S
         using (tablespace_name)
-order by free_gb desc;
+--order by free_gb desc
+order by tablespace_name asc
+;
