0ffc9510cbba9d384e0a32113610d8f005b0aa5c
NoP77svk <nop77svk@gmail.com>
2023-11-27 21:54:09 +0100

    ➕ perf/feature tests

---
+++ perf tests/[2023-11-27] fastest generate sequential numbers 1 to 2mil/01.1 test case draft.sql
@@ -0,0 +1,46 @@
+with xyz as (
+    select --+ no_merge
+        level - 1 as x
+    from dual
+    connect by level <= 2002225
+)
+select sum(x)
+from xyz
+;
+
+
+with xyz as (
+    select --+ no_merge
+        rownum - 1 as x
+    from dual
+    connect by level <= 2002225
+)
+select sum(x)
+from xyz
+;
+
+
+with xyz as (
+    select --+ no_merge
+        rownum - 1 as x
+    from xmltable('1 to 2002225')
+)
+select sum(x)
+from xyz
+;
+
+
+with xyz_1dim as (
+    select --+ no_merge
+        level - 1 as x
+    from dual
+    connect by level <= sqrt(2000000) + 1
+),
+xyz as (
+    select A.x * trunc(sqrt(2000000) + 1) + B.x as x
+    from xyz_1dim A
+        cross join xyz_1dim B
+)
+select sum(x)
+from xyz
+;
